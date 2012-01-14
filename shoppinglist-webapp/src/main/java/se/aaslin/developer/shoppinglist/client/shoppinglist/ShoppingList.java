2eb226336c9c402b28fd078273a62c7fb37029f7
Lars Åslin <lars.aslin@purplescout.se>
2012-01-14 23:14:37 +0100

    GWT in place

--- shoppinglist-webapp/src/main/java/se/aaslin/developer/shoppinglist/client/shoppinglist/ShoppingList.java
+++ shoppinglist-webapp/src/main/java/se/aaslin/developer/shoppinglist/client/shoppinglist/ShoppingList.java
@@ -1,12 +1,26 @@
 package se.aaslin.developer.shoppinglist.client.shoppinglist;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class ShoppingList implements EntryPoint {
-
+	
+	public interface ShoppingListUIBinder extends UiBinder<HTMLPanel, ShoppingList>{
+	}
+	
+	public ShoppingListUIBinder uiBinder = GWT.create(ShoppingListUIBinder.class);
+	
+	private String DIV_ID = "gwt_shoppingListPlaceHolder";
+	
 	@Override
 	public void onModuleLoad() {
+		RootPanel rootPanel = RootPanel.get(DIV_ID);
 		
+		HTMLPanel outerPanel = uiBinder.createAndBindUi(this);
+		rootPanel.add(outerPanel);
 	}
 
 }
--- shoppinglist-webapp/src/main/webapp/index.jsp
+++ shoppinglist-webapp/src/main/webapp/index.jsp
@@ -1,5 +1,12 @@
-<html>
+<?xml version="1.0" encoding="UTF-8" ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
 <body>
-<h2>Hello World!</h2>
+	<script type="text/javascript" src="<c:url value="/gwt.shoppinglist/gwt.shoppinglist.nocache.js" />"></script>
+
+	<div id="gwt_shoppingListPlaceHolder"></div>
 </body>
 </html>
