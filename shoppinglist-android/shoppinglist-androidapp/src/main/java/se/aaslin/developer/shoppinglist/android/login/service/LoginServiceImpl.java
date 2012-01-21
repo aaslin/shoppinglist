c3098b400af577a1b0f6bbdcb963c864830660fb
Lars Åslin <lars.aslin@purplescout.se>
2012-01-21 12:41:15 +0100

    rpc cont.

--- shoppinglist-android/shoppinglist-androidapp/src/main/java/se/aaslin/developer/shoppinglist/android/login/service/LoginServiceImpl.java
+++ shoppinglist-android/shoppinglist-androidapp/src/main/java/se/aaslin/developer/shoppinglist/android/login/service/LoginServiceImpl.java
@@ -1,11 +1,11 @@
 package se.aaslin.developer.shoppinglist.android.login.service;
 
-import com.gdevelop.gwt.syncrpc.SyncProxy;
+import se.aaslin.developer.robosync.SyncProxy;
 
 
 public class LoginServiceImpl implements LoginService{
 
-	LoginService loginService = (LoginService) SyncProxy.newProxyInstance(LoginService.class, "http://192.168.0.12:8080/shoppinglist/gwt.shoppinglist", "login");
+	LoginService loginService = (LoginService) SyncProxy.newProxyInstance(LoginService.class, "http://192.168.0.12:8080/shoppinglist/gwt.shoppinglist/", "login");
 
 	public boolean login(String uname, String pass) {
 		return loginService.login(uname, pass);
